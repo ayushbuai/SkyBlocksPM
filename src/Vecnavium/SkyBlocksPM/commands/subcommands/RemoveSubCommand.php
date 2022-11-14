77b41dea7a95725bedc6d79753f3b2fe6ae6421c
Nguyen Tan Phuong <37770098+Phuongaz@users.noreply.github.com>
2022-11-14 23:43:46 +0700

    Fix

--- src/Vecnavium/SkyBlocksPM/commands/subcommands/RemoveSubCommand.php
+++ src/Vecnavium/SkyBlocksPM/commands/subcommands/RemoveSubCommand.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Vecnavium\SkyBlocksPM\commands\subcommands;
 
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
 use Vecnavium\SkyBlocksPM\libs\CortexPE\Commando\args\RawStringArgument;
 use Vecnavium\SkyBlocksPM\libs\CortexPE\Commando\BaseSubCommand;
 use Vecnavium\SkyBlocksPM\SkyBlocksPM;
@@ -26,24 +28,34 @@ class RemoveSubCommand extends BaseSubCommand
 
         $player = SkyBlocksPM::getInstance()->getPlayerManager()->getPlayerByPrefix($sender->getName());
         $skyblock = SkyBlocksPM::getInstance()->getSkyBlockManager()->getSkyBlockByUuid($player->getSkyBlock());
+        if($skyblock->getLeader() !== $sender->getName())
+        {
+            $sender->sendMessage(TextFormat::colorize("&cBạn không phải là chủ của đảo này!"));
+            return;
+        }
+        if($memberRemove === $skyblock->getLeader())
+        {
+            $sender->sendMessage(TextFormat::colorize("&cBạn không thể xóa chủ đảo ra khỏi đảo!"));
+            return;
+        }
         $members = $skyblock->getMembers();
-        $members[] = $sender->getName();
-        if(isset($members[$memberRemove])){
-            unset($members[$memberRemove]);
+        if(in_array($memberRemove, $members)){
+            $key = array_search($memberRemove, $members);
+            unset($members[$key]);
             $skyblock->setMembers($members);
             foreach ($skyblock->getMembers() as $member)
             {
                 $mbr = SkyBlocksPM::getInstance()->getServer()->getPlayerByPrefix($member);
                 if ($mbr instanceof Player){
-                    $mbr->sendMessage(SkyBlocksPM::getInstance()->getMessages()->getMessage('remove-member', [
-                        "{PLAYER}" => $sender->getName()
-                    ]));
+                    $mbr->sendMessage(TextFormat::colorize("Đã xóa thành viên &e$memberRemove &rkhỏi hòn đảo của ". $skyblock->getLeader()));
                 }
             }
+            if(Server::getInstance()->getPlayerByPrefix($memberRemove) instanceof Player){
+                $mbr = Server::getInstance()->getPlayerByPrefix($memberRemove);
+                $mbr->sendMessage(TextFormat::colorize("Bạn đã bị xóa khỏi hòn đảo của &e". $skyblock->getLeader()));
+            }
         }else {
-            $sender->sendMessage(SkyBlocksPM::getInstance()->getMessages()->getMessage('member-not-found', [
-                "{PLAYER}" => $memberRemove
-            ]));
+            $sender->sendMessage(TextFormat::colorize("&cThành viên này không tồn tại trong đảo của bạn!"));
         }
     }
 
